a79de57a7208da56e66402c0f8ce6e5a7f34f439
IrinaBagriy <87617079+IrinaBagriy@users.noreply.github.com>
2022-12-31 20:38:49 +0300

    Add files via upload

---
+++ obliczenia.py
@@ -0,0 +1,17 @@
+#Program poda wynik roznych dzilan matematycznych 
+print("Poznasz wyniki dzialan matematycznych")
+print ("")
+print (" Wynik mnozenia dwoch liczb: 12345 wynosi : ")
+print(2345 * 7654)
+
+print(" Wynik dzielenia dwoch liczb: 5689 wynosi : ")
+print(5689 / 234)
+
+print (" Wynik dodawania dwoch liczb : 789 wynosi : ")
+print(2345 + 789)
+
+print(" Wynik dzielenia modulo dwoch liczb: 346 wynosi : ")
+print(3465 % 1025)
+
+input("\n\aby zakończyć program,naciśnij ENTER")
+
